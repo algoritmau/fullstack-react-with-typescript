18dc57b2631708557d3b3b439f52e1816bb1f0f1
Mauricio Paternina <spaceinvadev@icloud.com>
2022-05-13 16:41:29 -0500

    import Main into App

--- react-piano/src/App.tsx
+++ react-piano/src/App.tsx
@@ -1,14 +1,13 @@
-import styles from "./styles/App.module.css"
-import { Footer, Header } from "./components"
+import { Footer, Header, Main } from "./components"
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <Header />
-      <main className={styles.content} />
-      <Footer />
-    </div>
-  )
-}
+import styles from "./styles/App.module.css"
+
+const App = () => (
+  <div className={styles.app}>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
 
 export default App
